a22bf900eba8fcb469e13b53a05c3cb539aa3dcb
IagoAlm <iago.oli2000@gmail.com>
2022-12-13 01:27:47 -0300

    Verificação de movimentação (andamento)
    Retorno para o usuário quase finalizado.
    Necessita de uma função de filtro entre o usuário e o sistema.

--- dama.py
+++ dama.py
@@ -1,6 +1,7 @@
 import sys
+import string
 entr1 = 'aaaaaaaaaaaa........bbbbbbbbbbbb'
-entr = 'aaaaaaaaaaaa........abcdefghijkl'
+entr = 'aaaaaaaa......B.....acbdefghijkl'
 
 
 def lerInput(entrada):
@@ -34,27 +35,52 @@ def lerInput(entrada):
     return tabuleiro
 
 
+def verAcima(tabuleiro, linha, i):
+    if tabuleiro[linha-1][i-1] == '.' and i-1 >= 0:
+
+        esquerdaInt = str(linha+1)
+        esquerdaInt = esquerdaInt + str(i-1)
+        #esquerdaInt = esquerdaInt + list(string.ascii_lowercase)[i-1]
+    if tabuleiro[linha-1][i+1] == '.' and i-1 <= 7:
+        direitaInt = str(linha+1)
+        direitaInt = direitaInt + str(i+1)
+        #direitaInt = direitaInt + list(string.ascii_lowercase)[i+1]
+    # o retorno desses dados é 1 a mais na linha
+    return (esquerdaInt, direitaInt)
+
+
+def verAbaixo(tabuleiro, linha, i):
+    if tabuleiro[linha+1][i-1] == '.' and i-1 >= 0:
+
+        esquerdaInt = str(linha+2)
+        #esquerdaInt = esquerdaInt + list(string.ascii_lowercase)[i-1]
+        esquerdaInt = esquerdaInt + str(i-1)
+    if tabuleiro[linha+1][i+1] == '.' and i-1 <= 7:
+        direitaInt = str(linha+2)
+        direitaInt = direitaInt + str(i+1)
+    # o retorno desses dados é 1 a mais na linha
+    return (esquerdaInt, direitaInt)
+
+
 def verMovimento(tabuleiro, linha, jogador):
-    print(tabuleiro[4])
-    print(tabuleiro[5])
     if jogador == 'b':
         for i in range(len(tabuleiro[linha])):
-            if tabuleiro[linha][i] != ' ':
-                print(tabuleiro[linha][i])
-                if tabuleiro[linha-1][i-1] == '.' and i-1 >= 0:
-                    print(tabuleiro[linha][i], 'yes,', i)
-                if tabuleiro[linha-1][i+1] == '.' and i-1 <= 7:
-                    print(tabuleiro[linha][i], 'yes,', i+2)
-    """if jogador == 'a':
+            if tabuleiro[linha][i] == 'b':
+                print(verAcima(tabuleiro, linha, i))
+            if tabuleiro[linha][i] == 'B':
+                print(verAcima(tabuleiro, linha, i))
+                print(verAbaixo(tabuleiro, linha, i))
+
+    if jogador == 'a':
         for i in range(len(tabuleiro[5])):
-            if tabuleiro[5][i] != ' ':
-                print(tabuleiro[5][i])
-                if tabuleiro[5-1][i-1] == '.' and i-1 >= 0:
-                    print(tabuleiro[5][i], 'yes,', i)
-                if tabuleiro[5-1][i+1] == '.' and i-1 <= 7:
-                    print(tabuleiro[5][i], 'yes,', i+2)
-    """
+            if tabuleiro[linha][i] == 'a':
+                print(verAcima(tabuleiro, linha, i))
+            if tabuleiro[linha][i] == 'A':
+                print(verAcima(tabuleiro, linha, i))
+                print(verAbaixo(tabuleiro, linha, i))
 
 
 tabuleiro = lerInput(entr)
-verMovimento(tabuleiro)
+print(tabuleiro)
+
+verMovimento(tabuleiro, 5, 'b')
